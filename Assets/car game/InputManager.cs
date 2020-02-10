e82376083c291d9060ee168b2e1ea104015d054c
Bryanna N. Baker <bbaker17@butlercc.edu>
2020-02-10 13:53:52 -0600

    Half Way through class

---
+++ Assets/car game/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public PlayerMovement playerRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(playerRef == null) playerRef = GameObject.Find("Player").GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRef.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+    }
+}
---
+++ Assets/car game/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 10f, 
+                rotSpeed = 20f,
+                chosenDrag = 0.5f,
+                downForce = 10f;
+    private bool isGrounded;
+
+
+    void Start(){
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    public void Move(float h, float v){
+
+        if(isGrounded){
+            //horizontal rotates the car around the y axis
+            //vertical moves the car forwards and back on the z axis
+            rb.AddRelativeForce(0,0,v * speed * Time.deltaTime);
+            rb.AddRelativeTorque(0,h * rotSpeed * Time.deltaTime ,0);
+            rb.drag = chosenDrag;
+
+        }else{
+            rb.drag = 0f;
+            rb.AddRelativeForce(0, -downForce, 0);
+        }
+        
+
+
+    }
+
+    void OnTriggerStay(Collider other){
+        if(other.gameObject.CompareTag("Ground")){
+            isGrounded = true;
+        }
+
+    }
+    void OnTriggerExit(Collider other){
+        if(other.gameObject.CompareTag("Ground")){
+            isGrounded = false;
+        }
+
+        
+    }
+}
