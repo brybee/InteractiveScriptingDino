2741e092bb6dbcc42ad19cde8776030c36e22069
Bryanna Baker <brybaker33@gmail.com>
2020-03-08 03:36:26 -0500

    final

--- Assets/HitBox.cs
+++ Assets/HitBox.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HitBox : MonoBehaviour
 {
     
     public int health = 100;
+    public Text healthTxt;
     // Start is called before the first frame update
     void Start()
     {
+        healthTxt.text = "health = " + health;
         
     }
 
@@ -21,6 +24,12 @@ public class HitBox : MonoBehaviour
         if(other.CompareTag("EnemyBullet")){
             health -=10;
             Destroy(other.gameObject);
+            healthTxt.text = "health = " + health;
+        }
+        if (other.CompareTag("Health")){
+            health += 20;
+            Destroy(other.gameObject);
+            healthTxt.text = "health = " + health;
         }
 
     }
